0494b8ad7cd54f9ada971865a4546d34a3cb93d9
massi9106 <massi9106.ait@gmail.com>
2023-06-22 01:56:46 +0200

    add process config to encore

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
@@ -68,6 +70,10 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+Encore.configureDefinePlugin(options=>{
+    options["process.env"].API_URL = process.env.API_URL;
+    options["process.env"].HOST = process.env.HOST;
+})
 ;
 
 module.exports = Encore.getWebpackConfig();
