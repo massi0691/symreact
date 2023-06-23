8b675f3c20c3ad2781c683815df5b83f29953d3a
massi9106 <massi9106.ait@gmail.com>
2023-06-23 06:04:55 +0200

    index

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
+header("Access-Control-Allow-Origin: https://symreact-765ea167ead9.herokuapp.com");
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
