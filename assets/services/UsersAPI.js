722f79252af9b1330266b2b9d15e8fcf24a3de4c
massi9106 <massi9106.ait@gmail.com>
2023-06-23 06:17:13 +0200

    axios includ headers

--- assets/services/UsersAPI.js
+++ assets/services/UsersAPI.js
@@ -2,8 +2,19 @@ import axios from "axios";
 import {USERS_API} from "../js/config";
 
 function register(user) {
-    return axios.post(USERS_API, user);
+    return axios({
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        url: USERS_API,
+        data: user,
+        method: 'POST'
+    });
+
 }
+
 
 export default {
     register
